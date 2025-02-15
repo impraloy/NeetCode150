49905606b97c2a0c3d0f70692c9e7257e73b6438
impraloy <48139806+impraloy@users.noreply.github.com>
2025-02-14 23:35:16 -0500

    Day35_push

--- BackTracking/Word_Search.cpp
+++ BackTracking/Word_Search.cpp
@@ -1,39 +1,32 @@
 class Solution {
+private:
+   bool dfs(int pos, int i, int j, string &word, vector<vector<char>> &board){
+    char c = board[i][j];
+    if(c == '#') return false;
+    if(pos >= word.size() || c != word[pos]) return false;
+    if(pos == word.size()-1 && c == word[pos]) return true;
+
+    pos++;
+ 
+    board[i][j] = '#';
+    
+    if(i + 1 < board.size() && dfs(pos, i + 1, j, word, board)) return true;
+    if(i - 1 >= 0 && dfs(pos, i - 1, j, word, board)) return true;
+    if(j + 1 < board[0].size() && dfs(pos, i, j + 1, word, board)) return true;
+    if(j - 1 >= 0 && dfs(pos, i, j - 1, word, board)) return true;
+    board[i][j] = c;
+    return false;
+   }
+
 public:
-    int ROWS, COLS;
-    set<pair<int, int>> path;
-
     bool exist(vector<vector<char>>& board, string word) {
-        ROWS = board.size();
-        COLS = board[0].size();
-
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (dfs(board, word, r, c, 0)) {
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                if(dfs(0, i, j, word, board)){
                     return true;
-                }
+                    }
             }
         }
         return false;
-    }
-
-    bool dfs(vector<vector<char>>& board, string word, int r, int c, int i) {
-        if (i == word.length()) {
-            return true;
-        }
-
-        if (r < 0 || c < 0 || r >= ROWS || c >= COLS || 
-            board[r][c] != word[i] || path.count({r, c})) {
-            return false;
-        }
-
-        path.insert({r, c});
-        bool res = dfs(board, word, r + 1, c, i + 1) || 
-                   dfs(board, word, r - 1, c, i + 1) ||
-                   dfs(board, word, r, c + 1, i + 1) || 
-                   dfs(board, word, r, c - 1, i + 1);
-        path.erase({r, c});
-
-        return res;
     }
 };
