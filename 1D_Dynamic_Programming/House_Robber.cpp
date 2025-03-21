d9fd5b0ea25195f95aafadc4acb82713fa9ac90f
impraloy <48139806+impraloy@users.noreply.github.com>
2025-03-20 23:35:13 -0400

    Day67_push

--- 1D_Dynamic_Programming/House_Robber.cpp
+++ 1D_Dynamic_Programming/House_Robber.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int rob1 = 0, rob2 = 0;
+        int house1 = 0, house2 = 0;
 
-        for (int num : nums) {
-            int temp = max(num + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+        for(int num : nums){
+            int temp = max(house1 + num, house2);
+            house1 = house2;
+            house2 = temp;
         }
-        return rob2;
+        return house2;
+        
     }
 };
