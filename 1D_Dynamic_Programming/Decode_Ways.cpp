8350fba6e80cfc66bab6add4f0a91122071fecd6
impraloy <48139806+impraloy@users.noreply.github.com>
2025-03-25 23:37:00 -0400

    Day72_push

--- 1D_Dynamic_Programming/Decode_Ways.cpp
+++ 1D_Dynamic_Programming/Decode_Ways.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int dfs(int i, string& s) {
-        if (i == s.size()) return 1;
-        if (s[i] == '0') return 0;
-        
-        int res = dfs(i + 1, s);
-        if (i < s.size() - 1) {
-            if (s[i] == '1' || 
-               (s[i] == '2' && s[i + 1] < '7')) {
-                res += dfs(i + 2, s);
-            }
-        }
-        return res;
+    int numDecodings(string s) {
+        unordered_map<int, int> dp;
+        dp[s.size()] = 1;
+        return dfs(s, 0, dp);
     }
 
-    int numDecodings(string s) {
-        return dfs(0, s);
+private:
+    int dfs(string s, int i, unordered_map<int, int>& dp) {
+        if (dp.count(i)) {
+            return dp[i];
+        }
+        if (s[i] == '0') {
+            return 0;
+        }
+
+        int res = dfs(s, i + 1, dp);
+        if (i + 1 < s.size() && (s[i] == '1' || 
+            s[i] == '2' && s[i + 1] < '7')) {
+            res += dfs(s, i + 2, dp);
+        }
+        dp[i] = res;
+        return res;
     }
 };
